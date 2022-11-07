ad3a27951193894b8b2e17f46b06f642a3e96d2f
ricardothq <ricardothq@gmail.com>
2022-11-07 19:03:14 -0400

    Segundo commit

---
+++ resources/views/layouts/theme/styles.blade.php
@@ -0,0 +1,10 @@
+<script src="{{ asset('assets/js/loader.js') }}"></script>
+<link href="{{ asset('assets/css/loader.css') }}" rel="stylesheet" type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&display=swap" rel="stylesheet">
+<link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/plugins.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/structure.css') }}" rel="stylesheet" type="text/css" class="structure" />
+
+
+<link href="plugins/apex/apexcharts.css" rel="stylesheet" type="text/css">
+<link href="{{ asset('assets/css/dashboard/dash_1.css') }}" rel="stylesheet" type="text/css" class="dashboard-analytics" />
