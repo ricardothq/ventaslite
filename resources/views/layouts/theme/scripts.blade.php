18d44e79737762e1765dbe6963ebabde3e9a50dc
ricardothq <ricardothq@gmail.com>
2022-11-10 15:48:02 -0400

    4 commit

--- resources/views/layouts/theme/scripts.blade.php
+++ resources/views/layouts/theme/scripts.blade.php
@@ -2,7 +2,7 @@
 <script src="{{ asset('bootstrap/js/popper.min.js') }}"></script>
 <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('plugins/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
-<script src="{{ asset('assets/js/app.min.js') }}"></script>
+<script src="{{ asset('assets/js/app.js') }}"></script>
 <script>
     $(document).ready(function(){
         App.init();
@@ -11,7 +11,7 @@
 <script src="{{ asset('assets/js/custom.js') }}"></script>
 <script src="{{ asset('plugins/sweetalerts/sweetalert2.min.js') }}"></script>
 <script src="{{ asset('plugins/notification/snackbar/snackbar.min.js') }}"></script>
-<script src="{{ asset('plugins/nicescroll/nicescroll.min.js') }}"></script>
+<script src="{{ asset('plugins/nicescroll/nicescroll.js') }}"></script>
 <script src="{{ asset('plugins/currency/currency.js') }}"></script>
 
 <script>
--- resources/views/layouts/theme/sidebar.blade.php
+++ resources/views/layouts/theme/sidebar.blade.php
@@ -1,18 +1,17 @@
 <div class="sidebar-wrapper sidebar-theme">
-    <nav id="compactSidebar">
-
+<nav id="compactSidebar">
         <ul class="menu-categories">
-
             <li class="active">
                 <a href="#" class="menu-toggle" data-active="true">
                     <div class="base-menu">
                         <div class="base-icons">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-grid"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
                         </div>
-                        <span >CATEGORIAS</span>
+                        <span>CATEGORIAS</span>
                     </div>
                 </a>
-            </li>  
+            </li>
+
             <li class= "">
                 <a href="#" class="menu-toggle" data-active="false">
                     <div class="base-menu">
@@ -23,6 +22,7 @@
                     </div>
                 </a>
             </li>
+
             <li class="">
                 <a href="#" class="menu-toggle" data-active="false">
                     <div class="base-menu">
@@ -104,5 +104,5 @@
                 </a>
             </li>
         </ul>
-    </nav>
+</nav>
 </div>
