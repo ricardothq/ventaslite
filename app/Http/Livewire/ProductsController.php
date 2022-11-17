3146650a1233bc418b48b214448d9d2b3d6842c9
ricardothq <ricardothq@gmail.com>
2022-11-17 17:26:11 -0400

    3

--- app/Http/Livewire/ProductsController.php
+++ app/Http/Livewire/ProductsController.php
@@ -97,6 +97,79 @@ class ProductsController extends Component
         $this->resetUI();
         $this->emit('product-added', 'Producto Registrado');
     }
+
+    public function Edit(Product $product)
+    {
+        $this->selected_id = $product->id;
+        $this->name = $product->name;
+        $this->barcode = $product->barcode;
+        $this->cost = $product->cost;
+        $this->price = $product->price;
+        $this->stock = $product->stock;
+        $this->alerts = $product->alerts;
+        $this->categoryid = $product->category_id;
+        $this->image = null;
+
+        $this->emit('modal-show','show modal');
+    }
+
+
+
+    public function Update()
+    {
+        $rules =[
+            'name' => "required|min:3|unique:products,name,{$this->selected_id}",
+            'cost' => 'required',
+            'price' => 'required',
+            'stock' => 'required',
+            'alerts' => 'required',
+            'categoryid' => 'required|not_in:Elegir'
+        ];
+        $messages = [
+            'name.required' => 'Nombre del producto requerido' ,
+            'name.unique' => 'Ya existe el nombre del producto' ,
+            'name.min' => 'El nombre del producto debe tener al menos 3 caracteres' ,
+            'cost.required' => 'El costo es requerido' ,
+            'price.required' => 'El precio es requerido' ,
+            'stock.required' => 'El stock es requerido' ,
+            'alerts.required' => 'Ingresa el valor minimo en existencias' ,
+            'categoryid.not_in' => 'Elige un nombre de categoria diferente de Elegir' ,
+
+        ];
+
+        $this->validate($rules, $messages);
+
+        $product = Product::find($this->selected_id);
+        
+        $product->update([
+            'name' => $this->name,
+            'cost' => $this->cost,
+            'price' => $this->price,
+            'barcode' => $this->barcode,
+            'stock' => $this->stock,
+            'alerts' => $this->alerts,
+            'category_id' => $this->categoryid
+        ]);
+      
+        if($this->image)
+        {
+            $customFileName = uniqid() . '_.' . $this->image->extension();
+            $this->image->storeAs('public/products', $customFileName);
+            $imageTemp = $product->image; //imagen temporal
+            $product->image = $customFileName;
+            $product->save();
+
+            if($imageTemp !=null)
+            {
+                if(file_exists('storage/products/' . $imageTemp)){
+                    unlink('storage/products/' . $imageTemp);
+                }
+            }
+        }
+
+        $this->resetUI();
+        $this->emit('product-updated', 'Producto Actualizado');
+    }
     public function resetUI(){
 
     }
--- resources/views/livewire/products/component.blade.php
+++ resources/views/livewire/products/component.blade.php
@@ -44,7 +44,7 @@
                                 </td>
                                 <td class="text-center">
                                     <a href="javascript:void(0)"
-                                    wire:click.prevent="Edit({{$product->id}}"
+                                    wire:click.prevent="Edit({{$product->id}})"
                                     class="btn btn-dark mtmobile" title="Edit">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
