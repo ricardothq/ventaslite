f7e60bb78f8735b1778e0f811dada0d25f7f536e
ricardothq <ricardothq@gmail.com>
2022-11-18 19:00:48 -0400

    commit ventas 2.0

---
+++ resources/views/livewire/pos/partials/total.blade.php
@@ -0,0 +1,10 @@
+<div class="row">
+    <div class="col-sm-12">
+        <div>
+            <div class="connect-sorting">
+                <h5 class="text-center mb-3"></h5>
+
+            </div>
+        </div>
+    </div>
+</div>
