427376a436c06c88cdaf4074c00223ff790b7c0b
ricardothq <ricardothq@gmail.com>
2022-11-21 18:17:51 -0400

    Antes del Select2

--- app/Http/Livewire/PosController.php
+++ app/Http/Livewire/PosController.php
@@ -2,13 +2,159 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Denomination;
+use Darryldecode\Cart\Facades\CartFacade as Cart;
 use Livewire\Component;
 
 class PosController extends Component
 {
-    public $total, $cart =[];
+    public $total,$itemsQuantity, $efectivo, $change;
+
+    public function mount()
+    {
+        $this->efectivo =0;
+        $this->change =0;
+        $this->total = Cart::getTotal();
+        $this->itemsQuantity = Cart::getTotalQuantity();
+    }
     public function render()
     {
-        return view('livewire.pos.component')->extends('layouts.theme.app')->section('content');
+        return view('livewire.pos.component', [
+            'denominations' => Denomination::orderBy('value', 'desc')->get(),
+            'cart' => Cart::getContent()->sortBy('name')
+        ])
+        ->extends('layouts.theme.app')
+        ->section('content');
+    }
+
+    public function ACash($value)
+    {
+        $this->efectivo += ($value == 0 ? $this->total : $value);
+        $this->change = ($this->efectivo - $this->total); 
+    }
+
+    protected $listeners =[
+        'scan-code' => 'ScanCode',
+        'removeItem' => 'removeItem',
+        'clearCart' => 'clearCart',
+        'saveSale' => 'saveSale'
+    ];
+
+    public function ScanCode($barcode, $cant = 1)
+    {
+        $product = Product::where('barcode', $barcode)->first();
+
+        if($product == null || empty($empty))
+        {
+            $this->emit('scan-notfound','El producto no esta registrado');
+        } else {
+            if($this->InCart($product->id))
+            {
+                $this->increaseQty($product->id);
+                return;
+            }
+            if($product->stock < 1)
+            {
+                $this->emit('no-stock', 'Stock insuficiente :/');
+                return;
+            }
+
+            Cart::add($product->id, $product->name, $product->price, $cant, $product->image);
+            $this->total = Cart::getTotal();
+
+            $this->emit('scan-ok','Producto agregado');
+        }
+    }
+
+    public function InCart($productId)
+    {
+        $exist = Cart::get($productId);
+        if($exist)
+            return true;
+        else
+            return false;
+    }
+
+    public function increaseQty($productId, $cant = 1)
+    {
+        $title='';
+        $product = Product::find($productId);
+        $exist = Cart::get($productId);
+        if($exist)
+            $title = 'Cantidad actualizada';
+        else
+            $title = 'Producto agregado';
+        
+        if($exist)
+        {
+            if($product->stock < ($cant + $exist->quantity))
+            {
+                $this->emit('no-stock', 'Stock insuficiente :/');
+                return;
+            }
+        }
+
+        Cart::add($product->id, $product->name, $product->price, $cant, $product->image);
+
+        $this->total = Cart::getTotal();
+        $this->itemsQuantity = Cart::getTotalQuantity();
+
+        $this->emit('scan-ok', $title);
+    }
+
+    public function updateQty($product, $cant = 1)
+    {
+        $title ='';
+        $product = Product::find($productId);
+        $exist = Cart::get($productId);
+        if($exist)
+            $title = 'Cantidad actualizada';
+        else
+            $title ='Producto agregado';
+
+        if($exist)
+        {
+            if($product->stock < $cant)
+            {
+                $this->emit('no-stock', 'Stock insuficiente :/');
+                return;
+            }
+        }
+
+        $this->removeItem($productId);
+        if($cant > 0)
+        {
+            Cart::add($product->id, $product->name, $product->price, $cant, $product->image);
+
+            $this->total = Cart::getTotal();
+            $this->itemsQuantity = Cart::getTotalQuantity();
+
+            $this->emit('scan-ok', $title);
+        }
+    }
+
+    public function removeItem($productId)
+    {
+        Cart::remove($productId);
+
+        $this->total = Cart::getTotal();
+        $this->itemsQuantity = Cart::getTotalQuantity();
+
+        $this->emit('scan-ok', 'Producto Eliminado');
+    }
+
+    public function decreaseQty($productId)
+    {
+        $item = Cart::get($productId);
+        Cart::remove($productId);
+
+        $newQty = ($item->quantity) - 1;
+
+        if($newQty > 0)
+            Cart::add($item->id, $item->name, $item->price, $newQty, $item->attributes[0]);
+
+        $this->total = Cart::getTotal();
+        $this->itemsQuantity = Cart::getTotalQuantity();
+        $this->emit('scan-ok', 'Cantidad actualizada');
     }
 }
--- resources/views/livewire/pos/partials/total.blade.php
+++ resources/views/livewire/pos/partials/total.blade.php
@@ -2,7 +2,26 @@
     <div class="col-sm-12">
         <div>
             <div class="connect-sorting">
-                <h5 class="text-center mb-3"></h5>
+                <h5 class="text-center mb-3">RESUMEN DE VENTA</h5>
+
+                <div class="connect-sorting-content">
+                    <div class="card simple-title-task ui-sortable-handle">
+                        <div class="card-body">
+
+                            <div class="task-header">
+                                <div>
+                                    <h2>TOTAL: ${{number_format($total,2)}} </h2>
+                                    <input type="hidden" id="hiddenTotal" value="{{$total}}">
+                                </div>
+                                <div>
+                                    <h4 class="mt-3">Articulos: {{$itemsQuantity}}</h4>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
 
             </div>
         </div>
