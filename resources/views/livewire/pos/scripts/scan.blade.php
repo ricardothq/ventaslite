dfeb0df7a1a5da9cd7c5d35584a8c97347bafc72
ricardothq <ricardothq@gmail.com>
2022-11-22 19:05:52 -0400

    v.63

---
+++ resources/views/livewire/pos/scripts/scan.blade.php
@@ -0,0 +1,23 @@
+<script>
+
+try {
+    onScan.attach(document, {
+    suffixKeyCodes: [13],
+    onScan: function(barcode) {
+        console.log(barcode)
+        window.livewire.emit('scan-code', barcode)
+    }
+    onScanError: function(e) {
+        console.log(e)
+    }
+})
+
+    console.log('Scanner ready!')
+} catch(e){
+    console.log('Error de lectura: ', e)
+
+}
+
+
+
+</script>
--- resources/views/livewire/startpage/init.php
+++ resources/views/livewire/startpage/init.php
@@ -1,9 +1,9 @@
 <div class="row sales layout-top-spacing">
     <div class="col-sm-12">
-        <div class="widget-chart-one">
+        <div class="widget widget-chart-one">
             <div class="widget-heading">
                 <h4 class="car-title">
-                    <b>ComponentName | PageTitle </b>
+                    <b>componentName | pageTitle </b>
                 </h4>
                 <ul class="tabs tab-pills">
                     <li>
